6a24815c6e328a527997d0b173808963b8fd360e
anagashiro <a2020116626@estudiantes.upsa.edu.bo>
2021-10-12 20:38:35 -0400

    Agregar archivos de proyecto.

---
+++ MiPrimeraApi/Controllers/AdditionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimeraApi.Controllers
+{
+    public class Modelo
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdditionController : ControllerBase
+    {
+        [HttpGet]
+        public int Addition (int a, int b)
+        {
+            return (a + b);
+        }
+        [HttpPost]
+        public int Suma([FromBody] Modelo numeros  )
+        {
+            return (numeros.a + numeros.b);
+
+        }
+    }
+}
